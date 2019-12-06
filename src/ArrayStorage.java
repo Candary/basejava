3a6476df48627ccfc9fc984e814fffefd7335ac9
den1981 <eWp6LjTQAGtfYe5>
2019-12-06 15:56:01 +0600

    errors

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,17 +1,29 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
+//        this.storage[r] = Resume;
+//        this.r = storage;
+
+
+            this.storage[size] = r;
+            size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Arrays.toString(storage[size] );
+
     }
 
     void delete(String uuid) {
