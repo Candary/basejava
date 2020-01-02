b175ec3c25119ffd4512df0cd0ea448ebd9312b4
Candary <5h4m6l3r@gmail.com>
2020-01-02 15:09:28 +0700

    fix Storage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,59 +1,60 @@
-import java.util.Arrays;
-
-/**
- * Array based storage for Resumes
- */
-
-
-//        • Resume get
-//        метод должен вернуть Resume, а не uuid
-//        • метод size() должен возвращать количество резюме в массиве
-//        • метод getAll() должен возвращать все резюме, безе null
-//        ты ничего по сути не реализовал в этом дз
-
 public class ArrayStorage {
-    Resume[] storage = new Resume[5];
-    int size;
-
+    Resume[] storage = new Resume[10000];
+    int size=0;
     void clear() {
-        Arrays.fill(storage, 0, size, null);
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size=0;
     }
 
     void save(Resume r) {
-        this.storage[size] = r;
+        int index = 0;
+        if (size > 0) {
+            index = size;
+        }
+        storage[index] = r;
         size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size()-1; i++) {
+            if (storage[i].uuid.equals(uuid)){
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
-        int removeIndex = Arrays.toString(storage).indexOf(uuid);
-
-        for(int i = removeIndex; i < storage.length - 1; i++){
-            storage[i] = storage[i + 1];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+                break;
+            }
         }
+
+        while (index  != size-1) {
+            storage[index] = storage[index + 1];
+            index++;
+        }
+        storage[index] = null;
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int k=0;
-        String result="";
-        for(int i=0; i < storage.length; i++) {
-            if(storage[i]!=null) {
-                if(k > 0)
-                    result=", ";
-                result+=(storage[i]);
-                k++;
-            }
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++){
+            resumes[i] = storage[i];
         }
-        return Arrays.
+        return resumes;
     }
 
     int size() {
-        return storage.length;
+        return size;
     }
 }
