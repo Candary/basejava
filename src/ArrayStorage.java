e444223c01e79c316911eee926b3586c8e23a799
Candary <5h4m6l3r@gmail.com>
2020-01-08 13:54:41 +0700

    fix Storage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,25 +1,33 @@
+/*не забывай нажимать перед каждым коммитом для каждого класса, где были внесены изменения, Ctrl + Alt + L (это автоматическое форматирование кода)
+        в save удали index. Так и используй size, как есть
+        в get обращайся к size напрямую, а не через size()
+        i < size()-1
+        а зачем в методе get - 1?
+        в delete, если удалять нечего, то эти строки
+        storage[index] = null;        size--;
+        все равно выполняться. А так быть не должно*/
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int size=0;
+    int size = 0;
+
     void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
-        size=0;
+        size = 0;
     }
 
     void save(Resume r) {
-        int index = 0;
-        if (size > 0) {
-            index = size;
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
         }
-        storage[index] = r;
-        size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size()-1; i++) {
-            if (storage[i].uuid.equals(uuid)){
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
@@ -35,12 +43,14 @@ public class ArrayStorage {
             }
         }
 
-        while (index  != size-1) {
+        while (index != size - 1) {
             storage[index] = storage[index + 1];
             index++;
         }
+
         storage[index] = null;
         size--;
+
     }
 
     /**
@@ -48,7 +58,7 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         Resume[] resumes = new Resume[size];
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             resumes[i] = storage[i];
         }
         return resumes;
